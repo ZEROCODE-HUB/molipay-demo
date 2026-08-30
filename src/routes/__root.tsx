d1b6a9be93de28d9c2fc2aa53949797cbd90a8fd
unknown <andres@zerocode.la>
2026-08-29 23:22:17 -0500

    Use miniaturafinal.png as og:image thumbnail for social sharing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,7 +13,7 @@ import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { DemoModeProvider } from "../contexts/demo-mode";
 import { Toaster } from "@/components/ui/sonner";
-import ogImageSrc from "@/assets/miniatura.png";
+import ogImageSrc from "@/assets/miniaturafinal.png";
 
 function NotFoundComponent() {
   return (
